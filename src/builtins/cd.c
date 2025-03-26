c4112f0a29c9d76bdd9701b0f1c548522a83ce5d
Your Name <a.mura27400@gmail.com>
2025-03-26 13:35:17 +0100

    feat: builtins cd : add no arg and -

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -4,7 +4,14 @@
 int	cd(char *path)
 {
 	struct stat	sb;
+	char		cwd[PATH_MAX];
 
+	if (!path || *path == '\0' || strlen(path) == 0)
+		path = get_var_value("HOME");
+	if (!path || *path == '\0' || strlen(path) == 0)
+		return (0);
+	if (strncmp(path, "-", 1) == 0 && strlen(path) == 1)
+		path = get_var_value("OLDPWD");
 	if (access(path, F_OK) != 0)
 		return (perror("Le path n'existe pas"), 1);
 	if (stat(path, &sb) != 0)
@@ -13,8 +20,10 @@ int	cd(char *path)
 		return (fprintf(stderr, "Ce n'est pas un répertoire\n"), 1);
 	if (access(path, X_OK) != 0)
 		return (perror("Accès refusé au répertoire"), 1);
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+		cwd[0] = '\0';
+	set_var_value("OLDPWD", cwd);
 	if (chdir(path) != 0)
 		return (perror("Erreur lors du changement de répertoire"), 1);
-	
 	return (0);
 }
