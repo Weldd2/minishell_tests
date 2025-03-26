44e1642adffff0d1ea323058c7f6740c184e0564
Your Name <a.mura27400@gmail.com>
2025-03-26 12:29:47 +0100

    feat: improve readline prompt

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,10 +1,18 @@
 #include "builtins.h"
 
-void	cd(char *path) // TODO ongoing
+int	cd(char *path) // TODO ongoing
 {
-	if (access(path, R_OK | X_OK) == -1)
-		exit(EXIT_FAILURE);
-	if (chdir(path) == -1)
-		exit(EXIT_FAILURE);
-	return ;
+	struct stat sb;
+
+	if (access(path, F_OK) != 0)
+		return (perror("Le path n'existe pas"), 1);
+	if (stat(path, &sb) != 0)
+		return (perror("Erreur lors de l'obtention des informations"), 1);
+	if (!S_ISDIR(sb.st_mode))
+		return (fprintf(stderr, "Ce n'est pas un répertoire\n"), 1);
+	if (access(path, X_OK) != 0)
+		return (perror("Accès refusé au répertoire"), 1);
+	if (chdir(path) != 0)
+		return (perror("Erreur lors du changement de répertoire"), 1);
+	return (0);
 }
--- src/main.c
+++ src/main.c
@@ -1,26 +1,55 @@
 #include "minishell.h"
 
+t_ast_data	*init_ast_data(char *input)
+{
+	t_ast_data	*ast_data;
+
+	ast_data = mgc_alloc(sizeof(t_ast_data), 1);
+	ast_data->input = input;
+	ast_data->last_ope = NULL;
+	ast_data->prev = NULL;
+	ast_data->root = NULL;
+	return (ast_data);
+}
+
+static inline char *readline_prompt(char *buf, size_t size)
+{
+	char		cwd[PATH_MAX];
+	size_t		cwd_len;
+	size_t		prefix_len;
+	const char	*prefix;
+	
+	prefix = "minishell ";
+	prefix_len = strlen(prefix);
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+		cwd[0] = '\0';
+	cwd_len = strlen(cwd);
+	if (size < prefix_len + cwd_len + 2)
+		return (NULL);
+	memcpy(buf, prefix, prefix_len);
+	memcpy(buf + prefix_len, cwd, cwd_len);
+	buf[prefix_len + cwd_len] = ' ';
+	buf[prefix_len + cwd_len + 1] = '\0';
+	return (buf);
+}
+
 int	main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv, char **envp)
 {
 	char		*input;
 	t_ast_data	*ast_data;
+	char		prompt[PATH_MAX + 20];
 
 	ft_setenv(envp);
 	mgc_init();
-	set_var_value("HOME", "#");
 	while (1)
 	{
-		input = readline("minishell ");
+		input = readline(readline_prompt(prompt, PATH_MAX + 20));
 		if (input[0] != '\0')
 			add_history(input);
 		mgc_add_block(input);
 		if (strcmp(input, "exit") == 0)
 			break ;
-		ast_data = mgc_alloc(sizeof(t_ast_data), 1);
-		ast_data->input = input;
-		ast_data->last_ope = NULL;
-		ast_data->prev = NULL;
-		ast_data->root = NULL;
+		ast_data = init_ast_data(input);
 		print_ast(create_ast(ast_data), 0);
 	}
 	rl_clear_history();
