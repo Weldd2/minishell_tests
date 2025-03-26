09d01234f27771cd2317acfbbc0e76e1ff812694
Your Name <a.mura27400@gmail.com>
2025-03-26 16:00:34 +0100

    feat: improve cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,25 +1,42 @@
 #include "builtins.h"
 
+bool	check_permissions(char *path)
+{
+	struct stat	sb;
+
+	return (
+		(access(path, F_OK || X_OK) != 0
+		|| stat(path, &sb) != 0
+		|| !S_ISDIR(sb.st_mode))
+	);
+}
+
+bool	handle_char(char **path, char *test, char *var_name)
+{
+	int	path_len;
+	int	test_len;
+
+	path_len = strlen(*path);
+	test_len = strlen(test);
+	if (strncmp(*path, test, test_len) == 0 && path_len == test_len)
+		*path = get_var_value(var_name);
+	if (!*path || **path == '\0' || strlen(*path) == 0)
+		return (false);
+	return (true);
+}
 
 int	cd(char *path)
 {
-	struct stat	sb;
 	char		cwd[PATH_MAX];
 
 	if (!path || *path == '\0' || strlen(path) == 0)
 		path = get_var_value("HOME");
 	if (!path || *path == '\0' || strlen(path) == 0)
 		return (0);
-	if (strncmp(path, "-", 1) == 0 && strlen(path) == 1)
-		path = get_var_value("OLDPWD");
-	if (access(path, F_OK) != 0)
-		return (perror("Le path n'existe pas"), 1);
-	if (stat(path, &sb) != 0)
-		return (perror("Erreur lors de l'obtention des informations"), 1);
-	if (!S_ISDIR(sb.st_mode))
-		return (fprintf(stderr, "Ce n'est pas un répertoire\n"), 1);
-	if (access(path, X_OK) != 0)
-		return (perror("Accès refusé au répertoire"), 1);
+	if (!handle_char(&path, "--", "HOME") || !handle_char(&path, "-", "OLDPWD"))
+		return (0);
+	if (check_permissions(path))
+		return (perror("cd"), 1);
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
 		cwd[0] = '\0';
 	set_var_value("OLDPWD", cwd);
